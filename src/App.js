cf692f1bc21451e47ed854a5b18f4330693ee6ef
SaiChandra2896 <saichandra2896@gmail.com>
2019-06-06 13:24:17 +0530

    Basic React, Working with Lists and conditionals in a dynamic way, styling and using radium for styling

---
+++ src/App.js
@@ -0,0 +1,107 @@
+import React, { Component } from "react";
+import "./App.css";
+import Radium, { StyleRoot } from "radium";
+import Person from "./Person/Person";
+
+class App extends Component {
+  state = {
+    persons: [
+      { id: "asdas", name: "Sai", age: 24 },
+      { id: "edwe", name: "Pandu", age: 20 },
+      { id: "dcas", name: "Srujan", age: 24 }
+    ],
+    otherState: "Other",
+    showPersons: false
+  };
+
+  deletePersonHandler = personIndex => {
+    //const persons = this.state.slice();
+    const persons = [...this.state.persons]; //creating a copy of persons
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  };
+
+  nameChangeHandler = (event, id) => {
+    //get the id of person to update
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = { ...this.state.persons[personIndex] }; //creating the copy of the person
+    person.name = event.target.value; //updating the name
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person; //updating the array
+
+    this.setState({
+      persons: persons
+    });
+  };
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
+  render() {
+    const style = {
+      backgroundColor: "green",
+      color: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer",
+      ":hover": {
+        backgroundColor: "lightgreen",
+        color: "black"
+      }
+    };
+
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                name={person.name}
+                age={person.age}
+                click={() => this.deletePersonHandler(index)}
+                key={person.id}
+                change={event => this.nameChangeHandler(event, person.id)}
+              />
+            );
+          })}
+        </div>
+      );
+      style.backgroundColor = "red";
+      style[":hover"] = {
+        backgroundColor: "gold",
+        color: "black"
+      };
+    }
+
+    const classes = [];
+    if (this.state.persons <= 2) {
+      classes.push("red");
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push("bold");
+    }
+
+    return (
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, I'm a React App </h1>
+          <h1>Another</h1>
+          <p className={classes.join(" ")}> working</p>
+          <button style={style} onClick={this.togglePersonsHandler}>
+            Toggle Persons
+          </button>
+          {persons}
+        </div>
+      </StyleRoot>
+    );
+  }
+}
+
+export default Radium(App);

6032f3996a1761884c54fab3f88eae964620e2af
SaiChandra2896 <saichandra2896@gmail.com>
2019-06-07 16:39:24 +0530

    use setstate correctly

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,7 +16,8 @@ class App extends Component {
         { id: "dcas", name: "Srujan", age: 24 }
       ],
       otherState: "Other",
-      showPersons: false
+      showPersons: false,
+      toggleClicked: 0
     };
   }
   componentWillMount() {
@@ -79,7 +80,13 @@ class App extends Component {
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({ showPersons: !doesShow });
+    this.setState((prevState, props) => {
+      return {
+        showPersons: !doesShow,
+        toggleClicked: prevState.toggleClicked + 1
+      };
+      //if we want to setstate based on the previous state we have to pass a function as an arguement to set state taking prevstate and props as arguements
+    });
   };
   render() {
     console.log("[App.js]inside render");

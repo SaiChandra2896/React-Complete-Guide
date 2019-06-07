931c664e96b619193b020619e912ba7fc42feb4f
SaiChandra2896 <saichandra2896@gmail.com>
2019-06-07 17:00:28 +0530

    restrict the datatype of props using prop-type

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import Radium from "radium";
 import "./Person.css";
 import WithClass from "../../../hoc/WithClass";
@@ -37,5 +38,12 @@ class Person extends Component {
     );
   }
 }
+
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  change: PropTypes.func
+};
 
 export default Radium(Person);
